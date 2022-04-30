68b01b54054d5656eef3fc522bfdc8202c5f58b5
ilya <kassirovilya@gmail.com>
2022-04-30 13:44:20 +0300

    Variant with handle writing

--- SerializerTests/YourImplementation.cs
+++ SerializerTests/YourImplementation.cs
@@ -1,33 +1,100 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using System.Text.Unicode;
 using System.Threading.Tasks;
+using Newtonsoft.Json;
 using SerializerTests.Interfaces;
 using SerializerTests.Nodes;
 
 namespace SerializerTests.Implementations
 {
     //Specify your class\file name and complete implementation.
-    public class JohnSmithSerializer : IListSerializer
+    public class IliaKassSerializer : IListSerializer
     {
         //the constructor with no parameters is required and no other constructors can be used.
-        public JohnSmithSerializer()
+        public IliaKassSerializer()
         {
             //...
         }
 
-        public Task<ListNode> DeepCopy(ListNode head)
+        public async Task<ListNode> DeepCopy(ListNode head)
         {
-            throw new NotImplementedException();
+            var list = new List<ListNode>();
+            while (head.Next != null)
+            {
+                var node = new ListNode
+                {
+                    Data = head.Data
+                };
+                list.Add(node);
+            }
+            return await AddLinksToNodes(list);
         }
-
-        public Task<ListNode> Deserialize(Stream s)
+        public async Task<ListNode> Deserialize(Stream s)
         {
-            throw new NotImplementedException();
+            TextReader textReader = new StreamReader(s);
+            JsonReader reader = new JsonTextReader(textReader);
+            var node = new ListNode();
+            var list = new List<ListNode>();
+            while (await reader.ReadAsync())
+            {
+                switch (reader.TokenType)
+                {
+                    case JsonToken.StartObject:
+                        node = new ListNode();
+                        list.Add(node);
+                        break;
+                    case JsonToken.String:
+                        node.Data =  reader.Value.ToString();
+                        break;
+                }
+            }
+            return await AddLinksToNodes(list);
         }
-
-        public Task Serialize(ListNode head, Stream s)
+        public async Task Serialize(ListNode head, Stream s)
         {
-            throw new NotImplementedException();
+            StringBuilder sb = new StringBuilder();
+            StringWriter sw = new StringWriter(sb);
+            using JsonWriter writer = new JsonTextWriter(sw);
+            writer.Formatting = Formatting.Indented;
+            await writer.WriteStartArrayAsync();
+            while (head.Next != null)
+            {
+                await writer.WriteStartObjectAsync();
+                await writer.WritePropertyNameAsync("Data");
+                await writer.WriteValueAsync(head.Data);
+                await writer.WriteEndObjectAsync();
+                head = head.Next;
+            }
+            await writer.WriteStartObjectAsync();
+            await writer.WritePropertyNameAsync("Data");
+            await writer.WriteValueAsync(head.Data);
+            await writer.WriteEndObjectAsync();
+            await writer.WriteEndAsync();
+            var streamWriter = new StreamWriter(s);
+            await streamWriter.WriteAsync(sb);
+            await streamWriter.FlushAsync();
+            s.Position = 0;
+        }
+        private async Task<ListNode> AddLinksToNodes(List<ListNode> list)
+        {
+            if (list.Count==1)
+            {
+                return list[0];
+            }
+            list[0].Next = list[1];
+            for (int i = 1; i < list.Count-1; i++)
+            {
+                list[i].Next = list[i + 1];
+                list[i].Previous = list[i - 1];
+            }
+            list[^1].Previous = list[^2];
+            return list[0];
         }
     }
 }
--- TestApp/Program.cs
+++ TestApp/Program.cs
@@ -1,12 +1,72 @@
 ﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using SerializerTests.Implementations;
+using SerializerTests.Nodes;
 
 namespace TestApp
 {
     class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var node1 = new ListNode
+            {
+                Previous = null,
+                Next = null,
+                Random = null,
+                Data = 1.ToString()
+            };
+            var node2 = new ListNode
+            {
+                Previous = null,
+                Next = null,
+                Random = null,
+                Data = 2.ToString()
+            };
+            var node3 = new ListNode
+            {
+                Previous = null,
+                Next = null,
+                Random = null,
+                Data = 3.ToString()
+            };
+            var node4 = new ListNode
+            {
+                Previous = null,
+                Next = null,
+                Random = null,
+                Data = 4.ToString()
+            };
+           node1.Next = node2;
+           node2.Previous = node1;
+          // node2.Next = node3;
+            node3.Previous = node2;
+           node3.Next = node4;
+           node4.Previous = node3;
+
+            /*using (Stream s = new MemoryStream())
+            {
+                       IliaKassSerializer serializer = new IliaKassSerializer();
+                            
+                            await serializer.Serialize(node1, s);
+                            Console.WriteLine("Serialize");
+                            var firstnode = await serializer.Deserialize(s);
+                            Console.WriteLine(firstnode.Data);
+                            Console.WriteLine(firstnode.Next.Data);
+                            Console.WriteLine(firstnode.Next.Next.Data);
+                            Console.WriteLine(firstnode.Next.Next.Next.Data);
+            }*/
+
+            var options = new JsonSerializerSettings { PreserveReferencesHandling = PreserveReferencesHandling.Objects };
+            var a = JsonConvert.SerializeObject(node1, options);
+            Console.WriteLine(a);
+            var b = JsonConvert.DeserializeObject<ListNode>(a, options);
+            Console.WriteLine(b.Data);
+            Console.WriteLine(b.Next.Data);
+
+
         }
     }
 }
